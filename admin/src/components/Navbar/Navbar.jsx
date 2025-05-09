a6a8bd72d9e130918cea550fc318fd8af12d6c13
Ernest Wanjohi <ernest@example.com>
2025-05-09 14:22:47 +0300

    first commit

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import assets from "../../assets/assets";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="nav-Bar">
+      {/* <img className="logo" src={assets.logo} alt="logo" />
+      <img src="" alt="" /> */}
+      <p>rtfgcch</p>
+    </div>
+  );
+};
+
+export default Navbar;
